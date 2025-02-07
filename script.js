c2df55c0f5a04cdf458fba29fbc4a3b2e5cfe7cc
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-08 02:14:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let timerDisplay = document.getElementById("timer");
+let currentDate = new Date();
+ setInterval (
+	 function() {
+		currentDate = new Date();
+	 timerDisplay.innerHtml = currentDate.toLocalString();
+	 
+	 }, 1000;
+ )
